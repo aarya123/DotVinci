8efcc8fc69753c80613bf3fce9844b8cdf9af4e8
jethva <jethva@purdue.edu>
2014-09-16 20:29:08 -0400

    Now with the files XD

---
+++ pixelator/NegativeFilter.java
@@ -0,0 +1,14 @@
+package pixelator;
+
+public class NegativeFilter implements IFilter{
+
+	static final double MAX_PIXEL = 255.0;
+	
+	public Pixel filterPixel(Pixel pixel) {
+		double newRed = MAX_PIXEL - pixel.getRed();
+		double newGreen = MAX_PIXEL - pixel.getGreen();
+		double newBlue = MAX_PIXEL - pixel.getBlue();
+		return new Pixel(pixel.getX(), pixel.getY(), newRed, newGreen, newBlue);
+	}
+
+}
