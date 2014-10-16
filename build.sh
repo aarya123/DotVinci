07ebeee633ce99bd5caeaffc929e98a6eda8463b
jethva <jethva@purdue.edu>
2014-10-16 14:49:19 -0400

    added files

---
+++ build.sh
@@ -0,0 +1,2 @@
+javac -cp Sharing/commons-email-1.3.3.jar:Sharing/javax.mail.jar:. Main.java  Sharing/GmailShare.java Sharing/ShareInterface.java
+java -cp Sharing/commons-email-1.3.3.jar:Sharing/javax.mail.jar:. Main
