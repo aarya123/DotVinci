6bf7f4388ee0cb5164d4c4f32c7fd4588be7a536
Pranav Menon <pranavmenon2468@gmail.com>
2014-10-16 19:21:59 -0400

    Added Dan's bug back in

--- Engine.java
+++ Engine.java
@@ -118,8 +118,11 @@ public class Engine {
     }
 
     private void drawDot(Graphics g) {
-        int x = (int) (getImage().getWidth() * Math.random());
-        int y = (int) (getImage().getHeight() * Math.random());
+    	//bug
+    	int y = (int) (getImage().getWidth() * Math.random());
+        
+    	//bug
+    	int x = (int) (getImage().getHeight() * Math.random());
         Color color = new Color(getImage().getRGB(x, y));
         Pixel pixel = new Pixel(x, y, color.getRed(), color.getGreen(), color.getBlue());
         pixel = getFilter().filterPixel(pixel);
